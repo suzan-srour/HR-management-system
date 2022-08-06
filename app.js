8f36efa559d6e2b12cbaf3f6b2cfdab8f35e4956
suzan-srour <sroursuzan0@gmail.com>
2022-08-06 05:27:23 +0300

    A1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
-        const allUsers=[]; //array
-        counter=999;
+        let allUsers=[]; //array
+        let Id=999;
+        let net=0;
         function User(nameValue,idValue,departmentValue,levelValue,imagValue,salaryValue) 
     
         {
@@ -10,51 +11,108 @@
          this.Level=levelValue;
          this.imageURL =imagValue;
          this.Salary=salaryValue; 
-        
-         uniNum=function(counter){ 
-         this.EmployeeID=counter+1 ;
-          ++counter;} ;
-         
-          getRandomSalary=function(){ 
-          let Salary=Math.floor(Math.random() *(2000-1500)+1500);
-          let netSalary=Salary-(Salary*.075);
-          this.Salary=netSalary;
-          console.log("Employee Salary"+Salary);
-        } 
-          allUsers.push(this); /////////////////////////////////////////////////////////////////each obj we creat will be pushed to the array 
 
-        }//constuctor 
+        this.AddId=function(Id){
+          this.EmployeeID=Id;
+        };
+
+        this.Salary=function(salary){
+          this.Salary=salary;
+        };
+
+          allUsers.push(this); /////////////////////////////////////////////////////////////////each obj we creat will be pushed to the array 
+           } 
+        //constuctor 
+
+
+        AddId =function()
+        {Id=Id+1;
+        return Id;}
+
+       salary= function (x){
+         if (x=="Senior") 
+         { 
+          let salary=Math.floor(Math.random() *(2000-1500)+1500);
+          let net=salary-(salary*.075);
+           return net ; 
+         }
+         else if (x == "Mid-Senior")
+         {
+          let salary=Math.floor(Math.random() *(1500-1000)+1000);
+          let net=salary-(salary*.075);
+           return net ;
+          }
+         
+         else ( x=="Junior")
+         {
+          let salary=Math.floor(Math.random() *(1000-500)+500);
+          let net=salary-(salary*.075);
+          return net ;
+         }
+         
+         };
+
+
+
 
         document.write("<h1>Welcome To Our Management System</h1><br> <p>Hi hope you see it usefull</p>")
 
        User.prototype.print=function(){
-       console.log(`${this.FullName} :${this.Department} :${ this.getRandomSalary}`)  ;
-       document.write(`${this.FullName} :${this.Department} :${ this.getRandomSalary}`) ;}  /////////////////////// using prototype 
+        document.write(`<br>`);
+       document.write(
+        `Employee Name :  ${this.FullName}`,
+         `Department :   ${this.Department} `,
+         `Employee Salary   :${ this.Salary}`) ;
+
+         document.write(`<br>`);
+      } ;
+       
+       //document.write(`<img src=${this.imageURL}>`) ;
+
+
+        /////////////////////// using prototype 
       
 
 
-      let Ghazi=new User("Ghazi Samer","0000","Administration","Senior","./pic/man.jpg","0") ;//1
-
-      let Lana =new User("Lana Ali","0000","Finance","Senior","./pic/woman.jpg","0") ;//2
-
-      let Tamara=new User("Tamara Ayoub","0000","Marketing","Senior","./pic/woman.jpg","0") ;//3
-
-      let Safi=new User("Safi Waleed","0000","Administration","Mid-Senior","./pic/man.jpg","0") ;//4
-
-      let Rana=new User("Rana Salah","0000","Development","Junior","./pic/woman.jpg","0") ;//5
-
-      let Hadi=new User("Hadi Ahmad","0000","Finance","Mid-Senior","./pic/man.jpg","0") ;//6
-    
-      let Omar= new User("Omar Zaid","0000","Development","Senior","./pic/man.jpg","0") ;//7
+      let Ghazi=new User("Ghazi Samer","Administration","Senior","./pic/man.jpg") ;//1
+      let Lana =new User("Lana Ali","Finance","Senior","./pic/woman.jpg") ;//2
+      let Tamara=new User("Tamara Ayoub","Marketing","Senior","./pic/woman.jpg") ;//3
+      let Safi=new User("Safi Waleed","Administration","Mid-Senior","./pic/man.jpg") ;//4
+      let Rana=new User("Rana Salah","Development","Junior","./pic/woman.jpg") ;//5
+      let Hadi=new User("Hadi Ahmad","Finance","Mid-Senior","./pic/man.jpg") ;//6
+      let Omar= new User("Omar Zaid","Development","Senior","./pic/man.jpg") ;//7
 
     console.log(allUsers); 
-     Ghazi.print() ;
-     Lana.print() ;
-     Tamara.print() ;
-     Safi.print() ;
-     Rana.print() ;
-     Hadi.print();
-     Omar.print();
+
+     Ghazi.AddId(AddId());
+     Lana.AddId(AddId());
+     Tamara.AddId(AddId());
+     Safi.AddId(AddId()) ;
+     Rana.AddId(AddId()) ;
+     Hadi.AddId(AddId());
+     Omar.AddId(AddId());
+
+
+     Ghazi.Salary(salary(Ghazi.Level));
+     Lana.Salary(salary(Lana.Level));
+     Tamara.Salary(salary(Tamara.Level));
+     Safi.Salary(salary(Safi.Level));
+     Rana.Salary(salary(Rana.Level));
+     Hadi.Salary(salary(Hadi.Level)) ;
+     Omar.Salary(salary(Omar.Level)) ;
+
+
+    //document.write( `<p>${this.FullName}: ${this.Department} </p>`) ;
+    //document.write(`<img src=${this.imageURL}>`) ;
+
+
+Ghazi.print();
+Lana.print();
+Tamara.print();
+Safi.print() ;
+Rana.print() ;
+Hadi.print();
+Omar.print();
 
 
 
